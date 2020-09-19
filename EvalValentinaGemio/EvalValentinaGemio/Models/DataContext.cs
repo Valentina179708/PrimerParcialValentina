7c422b40fef26d065b3b883c0965b0ba42636cfb
Valentina <Valentina@DESKTOP-VV1TMQV>
2020-09-18 21:48:42 -0400

    DataContext creado

---
+++ EvalValentinaGemio/EvalValentinaGemio/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace EvalValentinaGemio.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+    }
+}
